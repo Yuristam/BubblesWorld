7c58421b5f33d2ab631f30405b50773546ed2acd
Yuristam <yuristamasanakunov@gmail.com>
2024-01-11 01:07:47 +0600

    added white background and more colors

--- BubbleWorldConsole/Program.cs
+++ BubbleWorldConsole/Program.cs
@@ -1,22 +1,37 @@
-﻿Console.CursorVisible = false;
+﻿Console.Title = "Bubbles";
+Console.CursorVisible = false;
 
+for (int i = 0; i < 3600; i++)
+{
+    // uncomment below if you want background color to be white
+    //Console.BackgroundColor = ConsoleColor.White;
+    Console.Write(' ');
+}
 Random rnd = new Random();
 
-while(true)
+while (true)
 {
     Console.SetCursorPosition(rnd.Next(0, 120), rnd.Next(0, 28));
-    int Colornum = rnd.Next(0, 7);
+    int Colornum = rnd.Next(0, 15);
     switch (Colornum)
     {
-        case 0: Console.ForegroundColor = ConsoleColor.Red; break;
+        case 0: Console.ForegroundColor = ConsoleColor.Black; break;
         case 1: Console.ForegroundColor = ConsoleColor.DarkBlue; break;
-        case 2: Console.ForegroundColor = ConsoleColor.DarkMagenta; break;
-        case 3: Console.ForegroundColor = ConsoleColor.Yellow; break;
-        case 4: Console.ForegroundColor = ConsoleColor.DarkGreen; break;
-        case 5: Console.ForegroundColor = ConsoleColor.DarkGray; break;
-        case 6: Console.ForegroundColor = ConsoleColor.DarkCyan; break;
+        case 2: Console.ForegroundColor = ConsoleColor.DarkGreen; break;
+        case 3: Console.ForegroundColor = ConsoleColor.DarkCyan; break;
+        case 4: Console.ForegroundColor = ConsoleColor.DarkRed; break;
+        case 5: Console.ForegroundColor = ConsoleColor.DarkMagenta; break;
+        case 6: Console.ForegroundColor = ConsoleColor.DarkYellow; break;
+        case 7: Console.ForegroundColor = ConsoleColor.Gray; break;
+        case 8: Console.ForegroundColor = ConsoleColor.DarkGray; break;
+        case 9: Console.ForegroundColor = ConsoleColor.Blue; break;
+        case 10: Console.ForegroundColor = ConsoleColor.Green; break;
+        case 11: Console.ForegroundColor = ConsoleColor.Cyan; break;
+        case 12: Console.ForegroundColor = ConsoleColor.Red; break;
+        case 13: Console.ForegroundColor = ConsoleColor.Magenta; break;
+        case 14: Console.ForegroundColor = ConsoleColor.Yellow; break;
+        case 15: Console.ForegroundColor = ConsoleColor.White; break;
     }
-    Console.WriteLine('o');
-    Thread.Sleep(100);
-    //Console.Clear();
+    Console.Write('o'); // change the symbol
+    Thread.Sleep(100);  // change the speed
 }
